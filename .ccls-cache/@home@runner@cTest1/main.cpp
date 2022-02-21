a9a99ebf920ea5070b7e0317d6c48e96a7b9a57d
Charlie Y <89798338+Cpyingling@users.noreply.github.com>
2022-02-21 09:43:02 +0000

    initial commit

--- .ccls-cache/@home@runner@cTest1/main.cpp
+++ .ccls-cache/@home@runner@cTest1/main.cpp
@@ -1,34 +1,12 @@
 /*Author Charles Yingling
 Date 2/21/22*/
-
-/*Create a replIT called Test1
-
-Body Mass Index is a simple calculation using a person's height and weight. The
-formula is BMI = kg/m2 where kg is a person's weight in kilograms and m2 is
-their height in metres squared.
-
-A BMI of 25.0 or more is overweight, while the healthy range is 18.5 to 24.9.
-Anything lower than 18.5 is underweight.
-
-Write a program that calculates BMI  by:
-
-a) asks for weight in kilograms and height in meters, and
-
-b) calculates the BMI
-
-c) announces if the person is overweight, healthy or underweight.
-
-d) asks "do you wish to calculate BMI for another person? y/n". If answer is
-"y", program will again go to step a.
-
-if the answer is "n", program will terminate.
-*/
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 int main() {
+  while(true) {
   int weight;
   double height;
   string answer;
@@ -47,11 +25,11 @@ int main() {
   }
   cout << "do you wish to calculate BMI for another person? y/n\n";
   cin>>answer;
-  while(true) {
-    if(answer = 'y'){
+    if(answer == "y"){
         continue;
-        }else if(answer ='n'){
+        }else if(answer =="n"){
         break;
         }
   }
+  return 0;
 }
